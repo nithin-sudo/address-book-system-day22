1f00c65d28dad7a44bd09638fe42de92485b8b9c
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-07-14 16:36:02 +0530

    Added: three classes service person and main for adding person contact.

--- src/com/bridgelabz/addressbook/Person.java
+++ src/com/bridgelabz/addressbook/Person.java
@@ -2,5 +2,84 @@ package com.bridgelabz.addressbook;
 
 public class Person
 {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private String email;
+    private String zip;
+    private long phoneNumber;
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString(){
+        return "[" +"firstName=" + firstName + '\'' + ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' + ", city='" + city + '\'' +
+                ", state='" +state + '\'' + ", zip='" + zip + '\'' +
+                ", phoneNo='" + phoneNumber+ '\'' + ", email='" + email + '\'' + ']';
+    }
 }
