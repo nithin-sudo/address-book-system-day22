1a142a51ee7febbf9134258c010870211f923e1c
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-07-14 19:37:16 +0530

    Added:addContact method to add the details of the person.

--- src/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/com/bridgelabz/addressbook/AddressBookMain.java
@@ -1,8 +1,31 @@
 package com.bridgelabz.addressbook;
+import java.util.Scanner;
 
 public class AddressBookMain
 {
-    public static void main(String[] args) {
-        System.out.println("Welcome to address book system");
+    public static void main(String[] args)
+    {
+        Service service = new Service();
+        boolean isExit = false;
+        while (!isExit)
+        {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter options 1.Add. 2.Display Contact. 3.Exit.");
+            int userInput = scanner.nextInt();
+            switch (userInput)
+            {
+                case 1:
+                    service.addNewContact();
+                    break;
+                case 2:
+                    service.displayList();
+                    break;
+                case 3:
+                    isExit = true;
+                    break;
+                default:
+                    System.out.println("Please enter valid input");
+            }
+        }
     }
 }
--- src/com/bridgelabz/addressbook/Service.java
+++ src/com/bridgelabz/addressbook/Service.java
@@ -1,6 +1,52 @@
 package com.bridgelabz.addressbook;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Service
 {
+    static Scanner scanner = new Scanner(System.in);
+    ArrayList<Person> personList = new ArrayList<>();
+    /**
+     * Asking user enter the details of the person.
+     */
+    public void addNewContact()
+    {
+        Person person =new Person();
+        System.out.println("Enter First name:");
+        person.setFirstName(scanner.next());
+
+        System.out.println("Enter Last Name:");
+        person.setLastName(scanner.next());
+
+        System.out.println("Enter Address:");
+        person.setAddress(scanner.next());
+
+        System.out.println("Enter City:");
+        person.setCity(scanner.next());
+
+        System.out.println("Enter State:");
+        person.setState(scanner.next());
+
+        System.out.println("Enter Zip:");
+        person.setZip(scanner.next());
+
+        System.out.println("Enter Email:");
+        person.setEmail(scanner.next());
+
+        System.out.println("Enter Phone:");
+        person.setPhoneNumber(scanner.nextLong());
+
+        personList.add(person);
+    }
+    /**
+     * displaying the list
+     */
+    public void displayList()
+    {
+        for (Person iterator :personList)
+        {
+            System.out.print(iterator);
+        }
+    }
 
 }
