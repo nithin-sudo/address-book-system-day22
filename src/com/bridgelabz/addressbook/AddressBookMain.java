f838ece8d44a253495f5ec048e3cc01e270d2550
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-07-19 00:57:24 +0530

    Added:sortByName method and sorting personList by alphabetical order using stream API.

--- src/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/com/bridgelabz/addressbook/AddressBookMain.java
@@ -10,7 +10,7 @@ public class AddressBookMain
         while (!isExit)
         {
             Scanner scanner = new Scanner(System.in);
-            System.out.println("\nEnter options \n1.Add. \n2.edit contact \n3.delete contact \n4.Display Contact. \n5.duplicate check \n6.search person in a city \n7.view person \n8.Exit.");
+            System.out.println("\nEnter options \n1.Add. \n2.edit contact \n3.delete contact \n4.Display Contact.\n5.search person in a city \n6.view person \n7.sort by name\n8.Exit.");
             int userInput = scanner.nextInt();
             switch (userInput)
             {
@@ -18,30 +18,33 @@ public class AddressBookMain
                     service.addNewContact();
                     break;
                 case 2:
-                    service.editContact();
+                    System.out.println("Enter the Address Book name");
+                    String bookName = scanner.next();
+                    service.editContact(bookName);
                     break;
                 case 3:
+                    System.out.println("Enter the Address Book name");
+                    String bookNameToDelete = scanner.next();
                     System.out.println("Enter the Name of the person do you wants to delete:");
                     String personName = scanner.next();
-                    service.deleteContact(personName);
+                    service.deleteContact(personName,bookNameToDelete);
                     break;
                 case 4:
                     service.displayList();
                     break;
                 case 5:
-                    System.out.println("Enter the Name of the person you wants to check, weather that person is already present:");
-                    String firstName = scanner.next();
-                    service.duplicateCheck(firstName);
-                    break;
-                case 6:
                     System.out.println("Enter the city to be searched:");
                     String searchCity = scanner.next();
                     service.searchPersonInACity(searchCity);
                     break;
-                case 7:
+                case 6:
                     System.out.println("enter the city of a person to be viewed:");
                     String viewCity = scanner.next();
                     service.viewPersonInACity(viewCity);
+                    break;
+                case 7:
+                    service.sortByName();
+                    break;
                 case 8:
                     isExit = true;
                     break;
